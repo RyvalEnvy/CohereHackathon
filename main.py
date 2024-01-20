0c0886b1a5aa0cce9f95c9a34cd9bfee1ed79b3a
Kiran Edwardson <kiranedwardson@Kirans-MacBook-Pro-2.local>
2024-01-20 10:14:59 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from src.return_similar_passages import return_most_relevant_passage
+
+app = FastAPI()
+
+
+class PassageRequest(BaseModel):
+    input_passage: str
+    patent_number: str
+
+
+@app.post("/similar_passages/")
+async def return_similar_passage(passageRequest: PassageRequest):
+    passage_request_dict = passageRequest.dict()
+    return return_most_relevant_passage(
+        passage_request_dict["input_passage"], passage_request_dict["patent_number"]
+    )
+
+
+@app.get("/health/")
+async def return_similar_passage():
+    return "I be runnin'"
